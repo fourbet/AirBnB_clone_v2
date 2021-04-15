082ec2aa3c2cd6fba464f493ca30b15e80d714b8
fourbet <ofourbet@yahoo.fr>
2021-04-15 14:28:11 +0000

    fix()

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,11 +4,13 @@
 from fabric.operations import local
 from datetime import datetime
 
+
 def do_pack():
     """generates a .tgz archive"""
     local("mkdir -p versions")
     path = "versions/web_static_{}.tgz".format(datetime.strftime(
-                                                   datetime.now(), "%Y%m%d%H%M%S"))
+                                               datetime.now(),
+                                               "%Y%m%d%H%M%S"))
     result = local("tar -cvzf {} web_static"
                    .format(path))
     if result.failed:
