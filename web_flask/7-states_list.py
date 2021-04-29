5f2460e7070330825d892c3bc80c18890762fc51
fourbet <ofourbet@yahoo.fr>
2021-04-29 16:33:16 +0000

    feat(): List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Web framework"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML"""
+    dict = storage.all(State)
+    return render_template('7-states_list.html', storage=dict)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
