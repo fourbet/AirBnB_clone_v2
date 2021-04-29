af6107ecb461f8a0df0e57d1f05eec0e5a720d7a
fourbet <ofourbet@yahoo.fr>
2021-04-29 09:36:20 +0000

    feat(): C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Web framework"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbtn():
+    """display Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """display C  followed by the value of the text variable"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return 'C %s' % text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
