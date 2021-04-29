645760ca19a2a3ab2fb76ddccdc6cff7725ccf0f
fourbet <ofourbet@yahoo.fr>
2021-04-29 09:08:46 +0000

    feat(): Hello Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Web framework"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbtn():
+    """display Hello HBNB!"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
