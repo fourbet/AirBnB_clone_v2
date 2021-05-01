6d2093e60267519331ec955323c0cf60747c222d
fourbet <ofourbet@yahoo.fr>
2021-05-01 10:49:58 +0000

    fix(): pep8 compliant

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,8 +4,8 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
@@ -13,10 +13,12 @@ def states_list():
     dict = storage.all(State)
     return render_template('7-states_list.html', storage=dict)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """ remove the current SQLAlchemy Session"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
